0b4fd8b8ad5f47b886d69b159c62b82dbf8312f0
Syzgyn <git@syzgyn.com>
2025-02-26 21:03:08 -0500

    Initial Commit

---
+++ app/Filament/Resources/ContentResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContentResource\Pages;
+use App\Filament\Resources\ContentResource\RelationManagers;
+use App\Models\Content;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use SkyRaptor\FilamentBlocksBuilder\Blocks;
+use SkyRaptor\FilamentBlocksBuilder\Forms\Components\BlocksInput;
+
+
+class ContentResource extends Resource
+{
+    protected static ?string $model = Content::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                BlocksInput::make('content')
+                    ->blocks(fn () => [
+                        Blocks\Layout\Card::block($form),
+                        Blocks\Typography\Heading::block($form),
+                        Blocks\Typography\Paragraph::block($form)
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContents::route('/'),
+            'create' => Pages\CreateContent::route('/create'),
+            'edit' => Pages\EditContent::route('/{record}/edit'),
+        ];
+    }
+}
